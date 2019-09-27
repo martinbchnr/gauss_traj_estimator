ed9a2ec2258b8e3a68eeed482650dd05da8f5f16
Martin Buchner <martin.buechner@tum.de>
2019-09-27 20:58:04 +0900

    initial commit

---
+++ src/gauss_traj_estimator.cpp
@@ -0,0 +1,43 @@
+#include "../include/gauss_traj_estimator.h"
+
+GaussTrajEstimator::GaussTrajEstimator()
+{
+    node_name = ros::this_node::getName();
+}
+
+void GaussTrajEstimator::subscribeViaPoints() {
+    if (!via_points_topic.empty())
+	{
+		ROS_INFO("[%s]: Subscribing to topic '%s'", node_name.c_str(), goal_pos_topic.c_str());
+		via_points_subscriber = node.subscribe(goal_pos_topic,1, &LocalPathPlaner::goal_pos_call_back, this);
+	}
+	else
+	{	
+		ROS_INFO("[%s]: Variable '%s' is empty", node_name.c_str(), goal_pos_topic.c_str());
+	}
+}
+
+void GaussTrajEstimator::subscribeDronePos() {
+
+}
+
+void GaussTrajEstimator::spin() {
+
+    ros::rate r(1);
+    while(ros::ok()) {
+        ros::spinOnce();
+        if(lost_track) {
+
+        }
+        r.sleep();
+    }
+}
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "gauss_traj_estimator");
+    GaussTrajEstimator gaussian_traj_node;
+    gaussian_traj_node.subscribe_to_topics();
+    gaussian_traj_node.set_publishers();
+    gaussian_traj_node.spin();
+}
